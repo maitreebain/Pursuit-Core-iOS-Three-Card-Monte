39e037bc37b1fb0b044459121f640199187ce170
Maitree Bain <maitreebain@Maitrees-MacBook-Pro.local>
2019-10-28 17:15:06 -0400

    working on switch cases

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+    @IBAction func cardBackRed(_ sender: UIButton) {
+        
+    let images = ["threeCard", "KingCard", "threeCard"]
+        
+        let card = [0,1,2]
+    let randomCard = card.randomElement()
+    // cardButton.setBackgroundImage(UIImage: named; "cardBackRed", state: .normal)
+    
 
+        switch sender.tag {
+    case 0:
+        randomcard = cardButton.setBackgroundImage(UIImage: named; "cardBackRed", state: .normal)
+            print("hello")
+        case 1:
+            print("")
+    default:
+        print("hg")
+
+    }
 }
 
+}
