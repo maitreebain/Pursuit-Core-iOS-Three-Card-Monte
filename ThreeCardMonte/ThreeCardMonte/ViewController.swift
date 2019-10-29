63b5c42cfba48a17e1bf17cf64121514a922708e
maitreebain <maitreebain@gmail.com>
2019-10-29 16:05:35 -0400

    finished basic card game

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,30 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    @IBOutlet weak var display: UILabel!
+    
+    @IBOutlet weak var card1: UIButton!
+    
+    @IBOutlet weak var card2: UIButton!
+    
+    @IBOutlet weak var card3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
+    
+    @IBAction func resetGame(_ reset: UIButton){
+        card1.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+        card2.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+        card3.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+
+        card1.isEnabled = true
+        card2.isEnabled = true
+        card3.isEnabled = true
+    }
+    
+    
     @IBAction func cardBackRed(_ sender: UIButton) {
         
-    let images = ["threeCard", "KingCard", "threeCard"]
+        let images = ["threeCard", "kingCard", "threeCard"]
         
-        let card = [0,1,2]
-    let randomCard = card.randomElement()
-    // cardButton.setBackgroundImage(UIImage: named; "cardBackRed", state: .normal)
+        let randomCard = images.randomElement() ?? "threeCard"
+        
+        switch sender.tag {
+        case 0:
+            sender.setBackgroundImage(UIImage(named: randomCard), for: .normal)
+            
+            if randomCard == "kingCard"{
+                display.text = "You won!"
+            } else {
+                display.text = "You lost"
+            }
+        case 1:
+            sender.setBackgroundImage(UIImage(named: randomCard), for: .normal)
+            if randomCard == "kingCard"{
+                display.text = "You won!"
+            } else {
+                display.text = "You lost"
+            }
+        case 2:
+            sender.setBackgroundImage(UIImage(named: randomCard), for: .normal)
+            if randomCard == "kingCard"{
+                display.text = "You won!"
+            } else {
+                display.text = "You lost"
+            }
+        default:
+            display.text = "Try again."
+        }
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
+    }
+    
+    
+        
+    }
+
+    
     
 
-        switch sender.tag {
-    case 0:
-        randomcard = cardButton.setBackgroundImage(UIImage: named; "cardBackRed", state: .normal)
-            print("hello")
-        case 1:
-            print("")
-    default:
-        print("hg")
-
-    }
-}
-
-}
